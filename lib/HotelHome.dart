cf14a2831963f4dcbae96864af298435aba8c0da
BHAGYASUJA123 <BHAGYASUJA123>
2023-04-28 14:51:48 +0530

    initial

--- lib/HotelHome.dart
+++ lib/HotelHome.dart
@@ -1,4 +1,3 @@
-
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
@@ -19,8 +18,9 @@ class BankingHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Colors.blueAccent,
       appBar: AppBar(
-        backgroundColor: Colors.red.shade900,
+        backgroundColor: Colors.blueAccent,
         elevation: 0,
         leading: const Icon(Icons.menu),
         actions: const [
@@ -45,27 +45,31 @@ class BankingHomeScreen extends StatelessWidget {
           SliverList(
             delegate: SliverChildListDelegate.fixed(
               [
-               // const AccountTopcard(),
                 Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(5),
                       child: SizedBox(
-                        height: 180,
+                        height: 150,
                         child: Card(
+                          color: Colors.red.shade900,
                           child: Padding(
                             padding: const EdgeInsets.all(20),
                             child: Row(
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
+                                Flexible(
+                                  flex: 1,
+                                  child:
                                 CircleAvatar(
                                   backgroundColor: Colors.red.shade900,
                                   radius: 60,
                                   child: const CircleAvatar(
                                     radius: 56,
                                     backgroundImage: NetworkImage(
-                                        'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80 '),
+                                        'https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'),
                                   ),
+                                ),
                                 ),
                                 Padding(
                                   padding: const EdgeInsets.all(8.0),
@@ -74,7 +78,7 @@ class BankingHomeScreen extends StatelessWidget {
                                     crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
                                       const Text(
-                                        'SHIHAB K SALEEM',
+                                        'SAAMUNATI BACHAT',
                                         style: TextStyle(
                                             fontWeight: FontWeight.bold, fontSize: 20),
                                       ),
@@ -102,7 +106,7 @@ class BankingHomeScreen extends StatelessWidget {
                   child: Row(
                     children: [
                       Image.asset(
-                        'images/bank.png',
+                        'images/pink-cosmos.png',
                         width: 40,
                       ),
                       const SizedBox(width: 15),
@@ -115,24 +119,24 @@ class BankingHomeScreen extends StatelessWidget {
                   ),
                 ),
 
-                // GridView.builder(
-                //   padding: const EdgeInsets.all(15),
-                //   itemCount: 6,
-                //   shrinkWrap: true,
-                //   physics: const NeverScrollableScrollPhysics(),
-                //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                //     crossAxisCount: 3,
-                //     mainAxisSpacing: 10,
-                //     crossAxisSpacing: 10,
-                //   ),
-                //   itemBuilder: (context, index) => IconCards(index: index),
-                // ),
+                GridView.builder(
+                  padding: const EdgeInsets.all(15),
+                  itemCount: 6,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) => IconCards(index: index),
+                ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 15),
                   child: Row(
                     children: [
                       Image.asset(
-                        'images/bank.png',
+                        'images/pink-cosmos.png',
                         width: 40,
                       ),
                       const SizedBox(width: 15),
@@ -141,14 +145,106 @@ class BankingHomeScreen extends StatelessWidget {
                         style: TextStyle(
                             fontSize: 18, fontWeight: FontWeight.bold),
                       ),
+
                     ],
                   ),
                 ),
+                ListView.separated(
+                    padding: const EdgeInsets.all(15),
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                    blurRadius: 10, offset: Offset(10, 10))
+                              ]),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 15,
+                                height: 100,
+                                color: Colors.red.shade900,
+                              ),
+                              const SizedBox(width: 15),
+                              Column(
+                                crossAxisAlignment:
+                                CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'CWDR/ \n974884/9874513365478965',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 15),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text('10-06-2019')
+                                ],
+                              ),
+                              const Spacer(),
+                              const Padding(
+                                padding: EdgeInsets.only(right: 10),
+                                child: Text(
+                                  'INR. 10,000.00',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    separatorBuilder: (context, index) => const SizedBox(
+                      height: 10,
+                    ),
+                    itemCount: 5)
+
               ],
             ),
           ),
         ],
       ),
     );
+  }
+}
+class IconCards extends StatelessWidget {
+  final int index;
+
+  const IconCards({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    List iconList = [
+      ['images/accounts.png', 'My Account'],
+      ['images/e.png', 'Load eSeva'],
+      ['images/payment.png', 'Payment'],
+      ['images/transfer.png', 'Fund Transfer'],
+      ['images/money-transfer.png', 'Schedule Payment'],
+      ['images/scan.png', 'Scan To Pay']
+    ];
+    return Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            iconList[index][0],
+            width: 50,
+          ),
+          Text(
+            iconList[index][1],
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+
   }
 }
--- lib/LoginUsingSqlite/registration.dart
+++ lib/LoginUsingSqlite/registration.dart
@@ -18,30 +18,146 @@ class _regsqlState extends State<regsql> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-         child:   Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              child: Column(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
+                children: [
                   Text(
-                    "Login Page",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    'Register new\naccount',
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 20,
                   ),
-                  TextField(
-                      obscureText: isPassword,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          fillColor: Color(0xfff3f3f4),
-                          filled: true))
+                  Image.asset(
+                    'images/play.png',
+                    width: 99,
+                    height: 4,
+                  ),
                 ],
               ),
-            )
-        )
+              SizedBox(
+                height: 48,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    TextField(
+                   //   controller: nameController,
+                decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.account_box_sharp),
+                      hintText: 'Name',
 
+                      suffixIcon: SizedBox(),
+                    ),),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    TextField(
+                      //   controller: nameController,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.account_box_sharp),
+                        hintText: 'Email',
+
+                        suffixIcon: SizedBox(),
+                      ),),
+                    // InputField(
+                    //   hintText: 'Email',
+                    //   controller: emailController,
+                    //   suffixIcon: SizedBox(),
+                    // ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    TextField(
+                      //controller: passwordController,
+                    //  obscureText: !passwordVisible,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.account_box_sharp),
+                      hintText: 'Password',
+
+
+                      //  onPressed: togglePassword,
+                      ),
+                    ),
+
+              SizedBox(
+                height: 32,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        //isChecked = !isChecked;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color:  Colors.transparent,
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      width: 20,
+                      height: 20,
+                      child: Icon(
+                        Icons.check,
+                        size: 20,
+                        color: Colors.white,
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'By creating an account, you agree to our',
+                       // style: regular16pt.copyWith(color: textGrey),
+                      ),
+                      Text(
+                        'Terms & Conditions',
+                       // style: regular16pt.copyWith(color: primaryBlue),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account? ",
+                   // style: regular16pt.copyWith(color: textGrey),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(
+                        context,
+                      );
+                    },
+                    child: Text(
+                      'Login',
+                      //style: regular16pt.copyWith(color: primaryBlue),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+     ]),
+    ))
     );
   }
 }
